d579c0eefd6efb591144bee40fea0eb86b45d01e
Darnion <Darnion@DESKTOP-RPF3CSO>
2022-11-19 15:26:10 +0300

    Добавьте файлы проекта.

--- FormArcanoid.cs
+++ FormArcanoid.cs
@@ -237,19 +237,6 @@ namespace Arcanoid
                 }
             }
 
-            if (score >= 1000)
-            {
-                timer.Stop();
-                if (DialogResult.Yes == MessageBox.Show("Ещё раз?", "Вы выиграли!", MessageBoxButtons.YesNo))
-                {
-                    Init();
-                }
-                else
-                {
-                    Close();
-                }
-            }
-
             map[ballY, ballX] = 0;
             IsCollideX();
             ballX += dirX;
